5ce3c8b6c0e3695628278abe12f1d48c3dd783ae
jrmuys <jrmuyskens@gmail.com>
2020-10-28 18:01:49 -0400

    connect to db

--- app.js
+++ app.js
@@ -9,6 +9,33 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+//Connect to database
+var pg = require('pg');
+//or native libpq bindings
+//var pg = require('pg').native
+
+var conString = process.env.ELEPHANTSQL_URL || `postgres://ruozhckz:${process.env.DB_PASSWORD}@lallah.db.elephantsql.com:5432/ruozhckz`;
+
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+    client.end();
+  });
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
