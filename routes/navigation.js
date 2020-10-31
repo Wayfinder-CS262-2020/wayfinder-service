7cd8c3f5afe5b96706f443da929aad7b2d847f26
jrmuys <jrmuyskens@gmail.com>
2020-10-31 19:54:26 -0400

    Added query and imports

--- routes/navigation.js
+++ routes/navigation.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var { app, client } = require('../app');
 
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+  client
+    .query(
+      `SELECT roomnumber FROM Building, Room
+                WHERE name = 'SB'
+                AND Building.name = Room.containingBuilding;`
+    )
+    .next((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.sendStatus(404);
+    });
 });
 
 module.exports = router;
