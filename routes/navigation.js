64e3c5efa31fd79fa1215bb848a484432e920d44
jrmuys <jrmuyskens@gmail.com>
2020-10-31 18:59:00 -0400

    added first test db query

--- routes/navigation.js
+++ routes/navigation.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var client = require('../app');
 
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/sbrooms', function (req, res, next) {
+  client
+    .many(
+      'SELECT roomnumber FROM Building, Room WHERE name = "SB" AND Building.name = Room.containingBuilding;'
+    )
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
