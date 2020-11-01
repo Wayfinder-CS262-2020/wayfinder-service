25884a3c5a2964dc102fa8f5f0dfedacd20577c1
jrmuys <jrmuyskens@gmail.com>
2020-10-31 20:06:53 -0400

    changed db connection

--- routes/navigation.js
+++ routes/navigation.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var { app, client } = require('../app');
+var { app, db } = require('../app');
 
 router.get('/', function (req, res, next) {
-  client
-    .query(
-      `SELECT roomnumber FROM Building, Room
-                WHERE name = 'SB'
-                AND Building.name = Room.containingBuilding;`
-    )
+  res.send(client);
+  db.many(
+    `SELECT roomnumber FROM Building, Room
+                  WHERE name = 'SB'
+                  AND Building.name = Room.containingBuilding;`
+  )
     .next((data) => {
       res.send(data);
     })
